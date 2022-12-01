6af27dc8c40dd041461136c0f8b81c62e743d526
sarwjeet424 <sarwjeet424@gmail.com>
2022-12-01 12:59:00 +0530

    changes

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -14,7 +14,7 @@ const bookController =async function (req,res){
         
         if(!title)  return res.status(400).send({status:false,message:"title is required"})
         if(!isValidString(title))  return res.status(400).send({status:false,message:"Please enter the valid title"})
-        let data1= await bookModel.findOne({title:title})
+        let data1= await bookModel.findOne({title:title })
         if(data1) return res.status(400).send({status:false,message:"This title is already exist in books Database"})
 
         if(!excerpt)  return res.status(400).send({status:false,message:"excerpt is required"})
@@ -22,7 +22,7 @@ const bookController =async function (req,res){
         
         if(!userId)  return res.status(400).send({status:false,message:"userId is required"})
         if(!isIdValid(userId)) return res.status(400).send({status:false,message:"Please enter the valid userId"})
-        let data2 = await userModel.findOne({_id:userId}) 
+        let data2 = await userModel.findOne({_id:userId, isDeleted:false}) 
         if(!data2)  return res.status(404).send({status:false,message:"This userId is doesn't exist in users Database"})
         
         if(!ISBN)  return res.status(400).send({status:false,message:"ISBN is required"})
@@ -52,13 +52,17 @@ const getBooks =async function (req,res){
 
     try{
         if(Object.keys(req.query).length==0){
+
             let data1= await bookModel.find({isDeleted:false}).select({title:1,excerpt:1,userId:1,category:1,releasedAt:1,reviews:1}).sort({title:1})
             if(data1.length==0) return res.status(404).send({status:false,message:"No books found"})
             return res.status(200).send({status:true, message:'Success',data:data1})
+
         }else{
+
             let data2= await bookModel.find({$and:[req.query,{isDeleted:false}]}).select({title:1,excerpt:1,userId:1,category:1,releasedAt:1,reviews:1}).sort({title:1})
             if(data2.length==0) return res.status(404).send({status:false,message:"No books found"})
             return res.status(200).send({status:true, message:'Success',data:data2})
+
         }
     }catch(error){
         return res.status(500).send({status:false,message:error.message})
@@ -76,7 +80,6 @@ const getBookReviewData = async function (req,res){
         if(!data2) return res.status(404).send({status:false,message:"book is already deleted or doesn't exist"})
 
         let data1= await reviewModel.find({bookId:data , isDeleted:false}).select({isDeleted:0,__v:0,createdAt:0,updatedAt:0})
-
         let obj={...data2._doc,reviewData:data1}
 
         return res.status(200).send({status:true, message:'Success',data:obj})
@@ -89,20 +92,16 @@ const getBookReviewData = async function (req,res){
 const updateBook = async function (req,res){
     try{
     let updatedBody = req.body;
-
-    if(Object.keys(req.params).length==0) return res.status(400).send({ status: false, message: "BookId is required in path params" });
     
     let bookId = req.params.bookId;
-    if(!isIdValid(req.params.bookId)) return res.status(400).send({ status: false, message: "BookId is invalid" });
+    if(!isIdValid(bookId)) return res.status(400).send({ status: false, message: "BookId is invalid" });
 
     let {title,excerpt,releasedAt, ISBN } = updatedBody
 
     if (Object.keys(updatedBody).length == 0) return res.status(400).send({ status: false, message: "Request body doesn't be empty" });
 
     let bookPresent = await bookModel.findById(bookId);
-
     if (!bookPresent) return res.status(400).send({ status: false, message: "Book id is Incorrect" });
-
     if (bookPresent["isDeleted"] == true) { return res.status(404).send({ status: false, message: "Book is already deleted" }) }
 
     let tit= await bookModel.findOne({title:title})
@@ -114,6 +113,7 @@ const updateBook = async function (req,res){
     let updatedBook = await bookModel.findOneAndUpdate({ _id: bookId }, { $set: {title, excerpt, releasedAt, ISBN }},{ new: true });
       
     res.status(200).send({ status: true, message: "Success", data: updatedBook });
+
     }catch(error){
         return res.status(500).send({status:false,message:error.message})
     }
@@ -135,7 +135,6 @@ const deleteBook = async function (req, res) {
       return res.status(500).send({ status: false, message: error.message });
     }
   }
-
 
 
 module.exports={bookController,getBooks,getBookReviewData,updateBook ,deleteBook}
